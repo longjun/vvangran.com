005f2fb571498025fea641bfda0accb1ddcd0204
Joe.Wang <joe@rethk.com>
2022-04-06 17:22:06 +0800

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <Head>
       <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="HandheldFriendly" content="True" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>LONGJUN.WANG</title>
